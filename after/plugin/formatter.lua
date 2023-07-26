d2ff76c642d1231a925ccaaa51f8fafb068f4a15
Νίκος Μαρουλάκης <n.maroulakis@itlab.gr>
2023-07-26 13:31:30 +0300

    formatter settings

--- after/plugin/formatter.lua
+++ after/plugin/formatter.lua
@@ -1,33 +1,41 @@
 -- https://github.com/mhartington/formatter.nvim
+local settings = {
+	lua = { require("formatter.filetypes.lua").stylua },
+	typescript = { require("formatter.filetypes.typescript").prettier },
+	json = { require("formatter.filetypes.json").fixjson },
+	html = { require("formatter.filetypes.html").htmlbeautify },
+	xml = { require("formatter.filetypes.html").htmlbeautify },
+
+	cs = {
+		function()
+			return {
+				exe = "dotnet",
+				args = {
+					"csharpier",
+				},
+				stdin = true,
+			}
+		end,
+	},
+
+	["*"] = {
+		require("formatter.filetypes.any").remove_trailing_whitespace,
+	},
+}
 
 require("formatter").setup({
 	logging = false,
 	log_level = vim.log.levels.WARN,
-	filetype = {
-		lua = { require("formatter.filetypes.lua").stylua },
-		typescript = { require("formatter.filetypes.typescript").prettier },
-		json = { require("formatter.filetypes.json").fixjson },
-
-		cs = {
-			function()
-				return {
-					exe = "dotnet",
-					args = {
-						"csharpier",
-					},
-					stdin = true,
-				}
-			end,
-		},
-
-		["*"] = {
-			require("formatter.filetypes.any").remove_trailing_whitespace,
-		},
-	},
+	filetype = settings,
 })
 
 vim.cmd([[autocmd BufWritePre * :Format]])
 vim.keymap.set("n", "<leader>f", function()
+	if settings[vim.g.filetype] ~= nil then
+		vim.cmd([[Format]])
+	else
+		vim.lsp.buf.format()
+	end
 	print("formatting")
 	vim.cmd([[Format]])
 end)
