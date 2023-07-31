b3ccbcaddf2bd0f9c4db9ac509f2928be7859a6f
Νίκος Μαρουλάκης <nlsnightmare@gmail.com>
2023-07-31 12:04:49 +0300

    updating formatter options

--- after/plugin/formatter.lua
+++ after/plugin/formatter.lua
@@ -1,8 +1,12 @@
 -- https://github.com/mhartington/formatter.nvim
+
 local settings = {
 	lua = { require("formatter.filetypes.lua").stylua },
 	typescript = { require("formatter.filetypes.typescript").prettier },
 	json = { require("formatter.filetypes.json").fixjson },
+	scss = { require("formatter.filetypes.css").prettier },
+	sh = { require("formatter.filetypes.sh").shfmt },
+	markdown = { require("formatter.filetypes.markdown").prettier },
 	html = { require("formatter.filetypes.html").htmlbeautify },
 	xml = { require("formatter.filetypes.html").htmlbeautify },
 
@@ -17,10 +21,19 @@ local settings = {
 			}
 		end,
 	},
-
-	["*"] = {
-		require("formatter.filetypes.any").remove_trailing_whitespace,
+	cs = {
+		function()
+			return {
+				exe = "dotnet",
+				args = {
+					"csharpier",
+				},
+				stdin = true,
+			}
+		end,
 	},
+
+	["*"] = { require("formatter.filetypes.any").remove_trailing_whitespace },
 }
 
 require("formatter").setup({
@@ -29,13 +42,12 @@ require("formatter").setup({
 	filetype = settings,
 })
 
-vim.cmd([[autocmd BufWritePre * :Format]])
 vim.keymap.set("n", "<leader>f", function()
-	if settings[vim.g.filetype] ~= nil then
+	if settings[vim.bo.filetype] ~= nil then
 		vim.cmd([[Format]])
 	else
 		vim.lsp.buf.format()
 	end
-	print("formatting")
-	vim.cmd([[Format]])
 end)
+
+vim.cmd([[autocmd BufWritePre * :Format]])
