391178fe642f1cb9b5505e12769730d44f03912c
Νίκος Μαρουλάκης <nlsnightmare@gmail.com>
2024-02-08 11:51:50 +0200

    lsp changes

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -26,8 +26,22 @@ lsp.configure("lua_ls", {
 
 lsp.configure("omnisharp", {
 	handlers = {
-		["textDocument/definition"] = omnisharp_extended.handler,
+		["textDocument/definition"] = function(...)
+			return require("omnisharp_extended").handler(...)
+		end,
 	},
+	keys = {
+		{
+			"gd",
+			function()
+				require("omnisharp_extended").telescope_lsp_definitions()
+			end,
+			desc = "Goto Definition",
+		},
+	},
+	enable_roslyn_analyzers = true,
+	organize_imports_on_format = true,
+	enable_import_completion = true,
 })
 
 lsp.configure("emmet_ls", {
