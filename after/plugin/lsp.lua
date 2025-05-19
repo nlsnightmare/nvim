ddcfdbbb29e4ecfde1248b31d5d86586cc75ef12
Νίκος Μαρουλάκης <nlsnightmare@gmail.com>
2025-05-19 23:53:10 +0300

    migrating to 0.11 lsp

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,235 +1,253 @@
-local lsp = require("lsp-zero")
-local util = require("lspconfig.util")
-local omnisharp_extended = require("omnisharp_extended")
+-- local lsp = require("lsp-zero")
+-- local util = require("lspconfig.util")
+-- local omnisharp_extended = require("omnisharp_extended")
 local telescope = require("telescope.builtin")
 
-lsp.preset("recommended")
+vim.opt.autoindent = true
+vim.opt.completeopt = { "menu", "menuone", "noselect", "noinsert", "fuzzy" }
+vim.opt.shortmess:append("c")
 
-lsp.ensure_installed({
-	"ts_ls",
-	"rust_analyzer",
-	"intelephense",
+vim.lsp.config('typescript-language-server', {
+	filetypes = { "javascript", "typescript" },
+	root_markers = { "package.json" } })
+vim.lsp.config('angular-language-server', {
+	filetypes = {"ts", "typescript"},
+	root_markers = { "angular.json" } })
+
+vim.lsp.enable({
+	"dartls",
 	"lua_ls",
+	"intelephense",
+	"html_ls",
 	"emmet_ls",
+	"omnisharp",
+	"angular-language-server",
+	"ts_ls"
 })
+vim.lsp.enable("text", false)
 
-lsp.configure("html_ls", {
-	filetypes = { "html", "cshtml", "blade", "htmlangular" },
-})
 
-lsp.configure("emmet_ls", {
-	filetypes = {
-		"css",
-		"eruby",
-		"html",
-		"cshtml",
-		"javascriptreact",
-		"htmlangular",
-		"less",
-		"sass",
-		"scss",
-		"pug",
-		"typescriptreact",
-		"blade",
-	},
-})
+-- vim.lsp.config('*', {
+--   capabilities = {
+--     textDocument = {
+--       semanticTokens = {
+--         multilineTokenSupport = true,
+--       }
+--     }
+--   },
+--   root_markers = { '.git' },
+-- })
 
--- Fix Undefined global 'vim'
-lsp.configure("lua_ls", {
-	settings = {
-		Lua = {
-			diagnostics = {
-				globals = { "vim" },
-			},
-		},
-	},
-})
+-- vim.lsp.config("html_ls", {
+-- 	filetypes = { "html", "cshtml", "blade", "htmlangular" },
+-- })
 
-lsp.configure("omnisharp", {
-	handlers = {
-		["textDocument/definition"] = function(...)
-			return require("omnisharp_extended").handler(...)
-		end,
-	},
-	keys = {
-		{
-			"gd",
-			function()
-				require("omnisharp_extended").telescope_lsp_definitions()
-			end,
-			desc = "Goto Definition",
-		},
-	},
-	enable_roslyn_analyzers = true,
-	organize_imports_on_format = true,
-	enable_import_completion = true,
-})
+-- vim.lsp.config("emmet_ls", {
+-- 	filetypes = {
+-- 		"css",
+-- 		"eruby",
+-- 		"html",
+-- 		"cshtml",
+-- 		"javascriptreact",
+-- 		"htmlangular",
+-- 		"less",
+-- 		"sass",
+-- 		"scss",
+-- 		"pug",
+-- 		"typescriptreact",
+-- 		"blade",
+-- 	},
+-- })
 
-lsp.configure("csharp_ls", {
-	filetypes = { "cs", "razor", "cshtml", "csproj" },
-})
+-- -- Fix Undefined global 'vim'
+-- vim.lsp.config("lua_ls", {
+-- 	settings = {
+-- 		Lua = {
+-- 			diagnostics = {
+-- 				globals = { "vim" },
+-- 			},
+-- 		},
+-- 	},
+-- })
 
--- Prevent the server from moving things around
-local cmp = require("cmp")
-local luasnip = require("luasnip")
-luasnip.config.setup({
-	enable_autosnippets = true,
-	region_check_events = "CursorHold,InsertLeave",
-	-- those are for removing deleted snippets, also a common problem
-	delete_check_events = "TextChanged,InsertEnter",
-})
+-- vim.lsp.config("omnisharp", {
+-- 	handlers = {
+-- 		["textDocument/definition"] = function(...)
+-- 			return require("omnisharp_extended").handler(...)
+-- 		end,
+-- 	},
+-- 	keys = {
+-- 		{
+-- 			"gd",
+-- 			function()
+-- 				require("omnisharp_extended").telescope_lsp_definitions()
+-- 			end,
+-- 			desc = "Goto Definition",
+-- 		},
+-- 	},
+-- 	enable_roslyn_analyzers = true,
+-- 	organize_imports_on_format = true,
+-- 	enable_import_completion = true,
+-- })
 
-local cmp_select = { behavior = cmp.SelectBehavior.Select }
-require("luasnip.loaders.from_snipmate").lazy_load({ paths = { "./snippets" } })
+-- vim.lsp.config("csharp_ls", {
+-- 	filetypes = { "cs", "razor", "cshtml", "csproj" },
+-- })
 
-local has_words_before = function()
-	unpack = unpack or table.unpack
-	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-end
+-- -- Prevent the server from moving things around
+-- local cmp = require("cmp")
+-- local luasnip = require("luasnip")
+-- luasnip.config.setup({
+-- 	enable_autosnippets = true,
+-- 	region_check_events = "CursorHold,InsertLeave",
+-- 	-- those are for removing deleted snippets, also a common problem
+-- 	delete_check_events = "TextChanged,InsertEnter",
+-- })
 
-local cmp_mappings = lsp.defaults.cmp_mappings({
-	["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
-	["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
-	["<Tab>"] = cmp.mapping(function(fallback)
-		if cmp.visible() then
-			-- no idea why complete doesnt work here
-			-- https://github.com/hrsh7th/nvim-cmp/wiki/Example-mappings#luasnip
-			cmp.select_next_item(cmp_select)
-			cmp.complete({ select = true })
-			-- cmp.select_next_item()
-			-- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
-			-- they way you will only jump inside the snippet region
-		elseif luasnip.expand_or_jumpable() then
-			luasnip.expand_or_jump()
-		elseif has_words_before() then
-			cmp.complete()
-		else
-			fallback()
-		end
-	end, { "i", "s" }),
-	["<C-Space>"] = cmp.mapping.complete(),
-})
--- cmp_mappings['<CR>'] = nil
+-- local cmp_select = { behavior = cmp.SelectBehavior.Select }
+-- require("luasnip.loaders.from_snipmate").lazy_load({ paths = { "./snippets" } })
 
-lsp.setup_nvim_cmp({
-	mapping = cmp_mappings,
-	-- make snippets have higher priority
-	sources = cmp.config.sources({
-		{ name = "luasnip", option = { show_autosnippets = true } },
-		{ name = "nvim_lsp" },
-	}, {
-		{ name = "path" },
-		-- { name = "buffer" },
-	}),
-	enabled = function()
-		-- disable completion in comments
-		local context = require("cmp.config.context")
-		-- keep command mode completion enabled when cursor is in a comment
-		if vim.api.nvim_get_mode().mode == "c" then
-			return true
-		else
-			return not context.in_treesitter_capture("string") and not context.in_syntax_group("String")
-		end
+-- local has_words_before = function()
+-- 	unpack = unpack or table.unpack
+-- 	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+-- 	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+-- end
+
+-- -- local cmp_mappings = lsp.defaults.cmp_mappings({
+-- -- 	["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+-- -- 	["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+-- -- 	["<Tab>"] = cmp.mapping(function(fallback)
+-- -- 		if cmp.visible() then
+-- -- 			-- no idea why complete doesnt work here
+-- -- 			-- https://github.com/hrsh7th/nvim-cmp/wiki/Example-mappings#luasnip
+-- -- 			cmp.select_next_item(cmp_select)
+-- -- 			cmp.complete({ select = true })
+-- -- 			-- cmp.select_next_item()
+-- -- 			-- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+-- -- 			-- they way you will only jump inside the snippet region
+-- -- 		elseif luasnip.expand_or_jumpable() then
+-- -- 			luasnip.expand_or_jump()
+-- -- 		elseif has_words_before() then
+-- -- 			cmp.complete()
+-- -- 		else
+-- -- 			fallback()
+-- -- 		end
+-- -- 	end, { "i", "s" }),
+-- -- 	["<C-Space>"] = cmp.mapping.complete(),
+-- -- })
+-- -- cmp_mappings['<CR>'] = nil
+
+-- -- lsp.setup_nvim_cmp({
+-- -- 	mapping = cmp_mappings,
+-- -- 	-- make snippets have higher priority
+-- -- 	sources = cmp.config.sources({
+-- -- 		{ name = "luasnip", option = { show_autosnippets = true } },
+-- -- 		{ name = "nvim_lsp" },
+-- -- 	}, {
+-- -- 		{ name = "path" },
+-- -- 		-- { name = "buffer" },
+-- -- 	}),
+-- -- 	enabled = function()
+-- -- 		-- disable completion in comments
+-- -- 		local context = require("cmp.config.context")
+-- -- 		-- keep command mode completion enabled when cursor is in a comment
+-- -- 		if vim.api.nvim_get_mode().mode == "c" then
+-- -- 			return true
+-- -- 		else
+-- -- 			return not context.in_treesitter_capture("string") and not context.in_syntax_group("String")
+-- -- 		end
+-- -- 	end,
+-- -- })
+
+-- require("lsp_signature").setup({ max_width = 1000 })
+
+vim.api.nvim_create_autocmd("LspAttach", {
+	desc = "LSP actions",
+	callback = function(event)
+		local client = vim.lsp.get_client_by_id(event.data.client_id)
+
+		-- 		if client:supports_method("textDocument/completion") then
+		-- 			vim.lsp.completion.enable(true, client.id, event.buf, { autotrigger = true })
+		-- 		end
+
+		local opts = { buffer = event.buf }
+
+		-- 		-- huh? looks like lsp disables autoindent
+		-- 		-- Open definition in vsplit
+
+		-- 		-- stylua: ignore start
+		vim.keymap.set("n", "gD", ":vsp<cr><C-]>", opts)
+		-- vim.keymap.set("n", "K",           vim.lsp.buf.hover,                   opts)
+		vim.keymap.set("n", "gr", telescope.lsp_references)
+		vim.keymap.set("n", "gd", telescope.lsp_definitions)
+		vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, opts)
+		vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, opts)
+		vim.keymap.set('n', '<leader>a', vim.lsp.buf.code_action, opts)
+		vim.keymap.set("n", "[d", vim.diagnostic.goto_next, opts)
+		vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
+		vim.keymap.set("n", "<leader>r",   vim.lsp.buf.rename,                  opts)
+		vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
+		vim.keymap.set("n", "<leader>f", function() vim.lsp.buf.format { async = true } end, opts)
+		-- 		-- stylua: ignore end
+
+		-- 		-- vim.keymap.set('i', "<C-p>", function() cmp.mapping.select_prev_item(cmp_select) end, opts);
+		-- 		-- ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+
+		-- 		-- if cmp.visible() then
+		-- 		-- 	-- no idea why complete doesnt work here
+		-- 		-- 	-- https://github.com/hrsh7th/nvim-cmp/wiki/Example-mappings#luasnip
+		-- 		-- 	cmp.select_next_item(cmp_select)
+		-- 		-- 	cmp.complete({ select = true })
+		-- 		-- 	-- cmp.select_next_item()
+		-- 		-- 	-- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+		-- 		-- 	-- they way you will only jump inside the snippet region
+		-- 		-- elseif luasnip.expand_or_jumpable() then
+		-- 		-- 	luasnip.expand_or_jump()
+		-- 		-- elseif has_words_before() then
+		-- 		-- 	cmp.complete()
+		-- 		-- else
+		-- 		-- 	fallback()
+		-- 		-- end
+		-- 		-- end, { "i", "s" }),
+		-- 		-- ["<C-Space>"] = cmp.mapping.complete(),
+		-- 		-- })
+		-- 		-- cmp_mappings['<CR>'] = nil
+
+		-- 		-- For now let Formatter.nvim handle this
+
+		-- 		if client.name == "omnisharp" then
+		-- 			local function toSnakeCase(str)
+		-- 				return string.gsub(str, "%s*[- ]%s*", "_")
+		-- 			end
+
+		-- 			local tokenModifiers = client.server_capabilities.semanticTokensProvider.legend.tokenModifiers
+		-- 			for i, v in ipairs(tokenModifiers) do
+		-- 				tokenModifiers[i] = toSnakeCase(v)
+		-- 			end
+		-- 			local tokenTypes = client.server_capabilities.semanticTokensProvider.legend.tokenTypes
+		-- 			for i, v in ipairs(tokenTypes) do
+		-- 				tokenTypes[i] = toSnakeCase(v)
+		-- 			end
+		-- 		end
+
+		-- 		-- make these options only work on buffers with active lsp
+		-- 		local opts = { buffer = bufnr, remap = false }
+
+		-- 		vim.keymap.set("n", "gd", function()
+		-- 			if client.name == "omnisharp" then
+		-- 				return omnisharp_extended.telescope_lsp_definitions()
+		-- 			end
+
+		-- 			return telescope.lsp_definitions()
+		-- 		end, opts)
 	end,
 })
 
-require("lsp_signature").setup({
-	max_width = 1000,
-})
-lsp.on_attach(function(client, bufnr)
-	--- Guard against servers without the signatureHelper capability
-	if client.server_capabilities.signatureHelpProvider then
-		-- require("lsp-overloads").setup(client, {
-		-- 	-- UI options are mostly the same as those passed to vim.lsp.util.open_floating_preview
-		-- 	ui = {
-		-- 		border = "single", -- The border to use for the signature popup window. Accepts same border values as |nvim_open_win()|.
-		-- 		height = nil, -- Height of the signature popup window (nil allows dynamic sizing based on content of the help)
-		-- 		width = nil, -- Width of the signature popup window (nil allows dynamic sizing based on content of the help)
-		-- 		wrap = true, -- Wrap long lines
-		-- 		wrap_at = nil, -- Character to wrap at for computing height when wrap enabled
-		-- 		max_width = nil, -- Maximum signature popup width
-		-- 		max_height = nil, -- Maximum signature popup height
-		-- 		-- Events that will close the signature popup window: use {"CursorMoved", "CursorMovedI", "InsertCharPre"} to hide the window when typing
-		-- 		close_events = { "CursorMoved", "BufHidden", "InsertLeave" },
-		-- 		focusable = true, -- Make the popup float focusable
-		-- 		focus = false, -- If focusable is also true, and this is set to true, navigating through overloads will focus into the popup window (probably not what you want)
-		-- 		offset_x = 0, -- Horizontal offset of the floating window relative to the cursor position
-		-- 		offset_y = 0, -- Vertical offset of the floating window relative to the cursor position
-		-- 		floating_window_above_cur_line = false, -- Attempt to float the popup above the cursor position
-		-- 		-- (note, if the height of the float would be greater than the space left above the cursor, it will default
-		-- 		-- to placing the float below the cursor. The max_height option allows for finer tuning of this)
-		-- 		silent = true, -- Prevents noisy notifications (make false to help debug why signature isn't working)
-		-- 		-- Highlight options is null by default, but this just shows an example of how it can be used to modify the LspSignatureActiveParameter highlight property
-		-- 		highlight = {
-		-- 			italic = true,
-		-- 			bold = true,
-		-- 			fg = "#ffffff",
-		-- 		},
-		-- 	},
-		-- 	keymaps = {
-		-- 		next_signature = "<C-j>",
-		-- 		previous_signature = "<C-k>",
-		-- 		next_parameter = "<C-l>",
-		-- 		previous_parameter = "<C-h>",
-		-- 		close_signature = "<A-s>",
-		-- 	},
-		-- 	display_automatically = true, -- Uses trigger characters to automatically display the signature overloads when typing a method signature
-		-- })
-		-- vim.api.nvim_set_keymap("n", "<C-h>", "<cmd>LspOverloadsSignature<CR>", { noremap = true, silent = false })
-	end
-
-	-- @see https://github.com/OmniSharp/omnisharp-roslyn/issues/2483#issuecomment-1546721190
-	if client.name == "omnisharp" then
-		local function toSnakeCase(str)
-			return string.gsub(str, "%s*[- ]%s*", "_")
-		end
-
-		local tokenModifiers = client.server_capabilities.semanticTokensProvider.legend.tokenModifiers
-		for i, v in ipairs(tokenModifiers) do
-			tokenModifiers[i] = toSnakeCase(v)
-		end
-		local tokenTypes = client.server_capabilities.semanticTokensProvider.legend.tokenTypes
-		for i, v in ipairs(tokenTypes) do
-			tokenTypes[i] = toSnakeCase(v)
-		end
-	end
-
-	-- make these options only work on buffers with active lsp
-	local opts = { buffer = bufnr, remap = false }
-
-	-- huh? looks like lsp disables autoindent
-	vim.opt.autoindent = true
-
-	vim.keymap.set("n", "gd", function()
-		if client.name == "omnisharp" then
-			return omnisharp_extended.telescope_lsp_definitions()
-		end
-
-		return telescope.lsp_definitions()
-	end, opts)
-
-	-- Open definition in vsplit
-	vim.keymap.set("n", "gD", ":vsp<cr><C-]>", opts)
-
-	vim.keymap.set("n", "gr", telescope.lsp_references)
-	-- stylua: ignore start
-	vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-	vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-	vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-	vim.keymap.set('n', '<leader>a', vim.lsp.buf.code_action)
-	vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-	vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-	vim.keymap.set("n", "<leader>r", function() vim.lsp.buf.rename() end, opts)
-	-- vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-	-- stylua: ignore end
-
-	-- For now let Formatter.nvim handle this
-	-- vim.keymap.set("n", "<leader>f", function() vim.lsp.buf.format { async = true } end, opts)
-end)
-
-lsp.setup()
-vim.diagnostic.config({ virtual_text = true })
+-- vim.diagnostic.config({
+-- 	virtual_text = true,
+-- 	underline = true,
+-- 	update_in_insert = true,
+-- })
 
 vim.api.nvim_create_user_command("EditSnippets", function()
 	require("luasnip.loaders").edit_snippet_files()
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,5 +1,11 @@
 return {
-	{ "folke/neodev.nvim", opts = {} },
+	{
+		"folke/neodev.nvim",
+		opts = {},
+		dependencies = {
+			{ "williamboman/mason-lspconfig.nvim", dependencies = { "mason-org/mason.nvim" } },
+		},
+	},
 	{
 		"stevearc/dressing.nvim",
 		config = function()
@@ -65,29 +71,99 @@ return {
 	{ "folke/todo-comments.nvim", dependencies = "nvim-lua/plenary.nvim" },
 	{ "jghauser/mkdir.nvim" },
 
+	-- LSP Support
 	{
-		"VonHeikemen/lsp-zero.nvim",
-		branch = "v1.x",
+		"mason-org/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+
+	{
+		"saghen/blink.cmp",
+		event = "InsertEnter",
+		-- use a release tag to download pre-built binaries
+		version = "1.*",
+		---@module 'blink.cmp'
+		---@type blink.cmp.Config
+		opts = {
+			keymap = {
+				preset = "enter",
+				["<Tab>"] = { "select_next", "fallback" },
+				["<S-Tab>"] = { "select_prev", "fallback" },
+			},
+			snippets = { preset = "luasnip" },
+
+			sources = {
+				default = { "lsp", "snippets" },
+				providers = {
+					snippets = { min_keyword_length = 0 },
+				},
+			},
+
+			signature = { enabled = true },
+			appearance = { nerd_font_variant = "mono" },
+
+			-- (Default) Only show the documentation popup when manually triggered
+			completion = {
+				menu = {
+					draw = {
+						columns = {
+							{ "kind_icon", "label", "label_description", gap = 2 },
+						},
+						padding = { 1, 1 }, -- padding only on right side
+					},
+				},
+
+				trigger = {
+					show_on_keyword = true,
+					show_on_trigger_character = true,
+					show_on_insert_on_trigger_character = true,
+					show_on_accept_on_trigger_character = true,
+				},
+
+				ghost_text = {
+					enabled = false,
+				},
+				documentation = {
+					auto_show = true,
+					auto_show_delay_ms = 0,
+				},
+			},
+
+			-- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
+			-- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+			-- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+			--
+			-- See the fuzzy documentation for more information
+			fuzzy = { implementation = "prefer_rust_with_warning" },
+		},
+		opts_extend = { "sources.default" },
+
 		dependencies = {
-			-- LSP Support
-			"neovim/nvim-lspconfig",
-			{ "williamboman/mason.nvim", build = ":MasonUpdate" },
-			"williamboman/mason-lspconfig.nvim",
-
-			"Hoffs/omnisharp-extended-lsp.nvim",
-
-			-- Autocompletion
-			"hrsh7th/nvim-cmp", -- Required
-			"hrsh7th/cmp-nvim-lsp", -- Required
-			"hrsh7th/cmp-buffer", -- Optional
-			"hrsh7th/cmp-path", -- Optional
-			"saadparwaiz1/cmp_luasnip", -- Optional
-			"Issafalcon/lsp-overloads.nvim",
 			"ray-x/lsp_signature.nvim",
-
-			"hrsh7th/cmp-nvim-lua", -- Optional
-
-			"L3MON4D3/LuaSnip",
+			"Hoffs/omnisharp-extended-lsp.nvim",
+			"Issafalcon/lsp-overloads.nvim",
+			{
+				"L3MON4D3/LuaSnip",
+				version = "v2.*",
+				build = "make install_jsregexp",
+				config = function()
+					require("luasnip.loaders.from_snipmate").lazy_load()
+					require("luasnip").setup({
+						enable_autosnippets = true,
+					})
+				end,
+			},
 		},
 	},
+
+	"neovim/nvim-lspconfig",
+
+	-- Autocompletion
+	-- "hrsh7th/nvim-cmp", -- Required
+	-- "hrsh7th/cmp-nvim-lsp", -- Required
+	-- "hrsh7th/cmp-buffer", -- Optional
+	-- "hrsh7th/cmp-path", -- Optional
+	-- "saadparwaiz1/cmp_luasnip", -- Optional
 }
