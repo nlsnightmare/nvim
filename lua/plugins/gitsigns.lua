eb67bf9247f55b967375e1d8f24be52a318db092
Νίκος Μαρουλάκης <nlsnightmare@gmail.com>
2025-07-29 00:59:47 +0300

    adding nice bindings

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,4 +1,22 @@
 return {
 	"lewis6991/gitsigns.nvim",
 	opts = {},
+	keys = {
+		{
+			"[c",
+			function()
+				require("gitsigns").nav_hunk("next")
+			end,
+			mode = "n",
+			desc = "Next Change",
+		},
+		{
+			"]c",
+			function()
+				require("gitsigns").nav_hunk("prev")
+			end,
+			mode = "n",
+			desc = "Previous Change",
+		},
+	},
 }
